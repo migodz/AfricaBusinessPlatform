2f5b883e933112a920ac1c17347e6aea7a4ebef0
migodz <julian.zhuu@outlook.com>
2022-04-12 17:34:53 -0500

    modify login controller

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, EmailField, ValidationError
+from wtforms import Form, StringField, EmailField, ValidationError, IntegerRangeField
 from wtforms.validators import Length, Regexp, Email
 from models import User
 
@@ -7,7 +7,7 @@ class SignupForm(Form):
     usr = StringField(validators=[Length(min=5, max=25)])
     pwd = StringField(validators=[Regexp(r"^(?=.*[A-Za-z])(?=.*\d).{8,40}$")])
     email = EmailField(validators=[Email()])
-    nknm = StringField(validators=[Length(min=5, max=25)])
+    nknm = StringField()
     type = StringField(validators=[Length(min=3, max=3)])
 
     def validate_usr(self, field):
@@ -27,6 +27,13 @@ class SignupForm(Form):
         if not type_data in ('usr', 'cpn', 'cbr'):
             raise ValidationError("unknown user type!")
 
+    def validate_nknm(self, field):
+        nknm_data = field.data
+        if nknm_data == '':
+            return
+        if len(nknm_data) < 2 or len(nknm_data) > 15:
+            raise ValidationError("illegal nickname!")
+
 
 class LoginForm(Form):
     usr = StringField(validators=[Length(min=5, max=25)])
@@ -40,3 +47,14 @@ class NewPasswordForm(Form):
 
 class EmailForm(Form):
     email = EmailField(validators=[Email()])
+
+
+class GetCompaniesForm(Form):
+    limit = IntegerRangeField(min=1, max=30)
+    offset = IntegerRangeField(min=0)
+
+
+class GetChambersForm(Form):
+    limit = IntegerRangeField(min=1, max=30)
+    offset = IntegerRangeField(min=0)
+
